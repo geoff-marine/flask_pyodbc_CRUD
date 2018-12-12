7e0d71019eed7fbbbe1629f343462e64b26e43e0
Geoff Irwin <girwin@Marine.ie>
2018-12-12 16:50:35 +0000

    Add project files.

---
+++ flask_pyodbc_CRUD/app.py
@@ -0,0 +1,39 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask import jsonify
+import pyodbc
+
+app = Flask(__name__)
+api = Api(app)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+server = 'vminformdev01' 
+database = 'GI_VS_SC_Test' 
+
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
+cursor = cnxn.cursor()
+
+class HelloWorld(Resource):
+    def get(self):
+        cursor.execute("SELECT * FROM FOO;")       
+        columns = [column[0] for column in cursor.description]
+        results = []
+        for row in cursor.fetchall():
+            results.append(dict(zip(columns, row)))
+        return jsonify(results)
+        
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
