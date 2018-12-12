c76e4cf98f724abecde2113538f9c0c17c115c19
Geoff Irwin <girwin@Marine.ie>
2018-12-12 17:23:40 +0000

    foo and foos

--- flask_pyodbc_CRUD/app.py
+++ flask_pyodbc_CRUD/app.py
@@ -4,7 +4,7 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource
 from flask import jsonify
 import pyodbc
 
@@ -20,7 +20,7 @@ database = 'GI_VS_SC_Test'
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
 cursor = cnxn.cursor()
 
-class HelloWorld(Resource):
+class FOOS(Resource):
     def get(self):
         cursor.execute("SELECT * FROM FOO;")       
         columns = [column[0] for column in cursor.description]
@@ -29,8 +29,18 @@ class HelloWorld(Resource):
             results.append(dict(zip(columns, row)))
         return jsonify(results)
         
+class FOO(Resource):
+    def get(self, id):
+        cursor.execute("SELECT * FROM FOO WHERE fooKey =" + id)
+        columns = [column[0] for column in cursor.description]
+        results = []
+        for row in cursor.fetchall():
+            results.append(dict(zip(columns, row)))
+        return jsonify(results)
 
-api.add_resource(HelloWorld, '/')
+
+api.add_resource(FOO, '/FOO/<id>')
+api.add_resource(FOOS,'/FOOS')
 
 if __name__ == '__main__':
     app.run(debug=True)
